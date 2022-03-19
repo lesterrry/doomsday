3d20b1d19152d18227ac86a558f01bd1eb081b2f
Lester <me@aydar.media>
2022-03-19 22:38:18 +0300

    Create message.php

---
+++ message.php
@@ -0,0 +1,28 @@
+<?php
+define("PLAIN_PATH", "PATH TO LOG FILE");
+define("PICS_FOLDER_PATH", "PATH TO PICS FOLDER");
+require __DIR__ . "/auth.php";
+if (isset($_POST['message']) && isset($_POST['key'])) {
+	$msg = $_POST['message'];
+	$key = $_POST['key'];
+	$username = getusername($key);
+	$msg = strip_tags($msg);
+	if(keyvalid($key)) {
+		$picfrmt = "";
+		if (isset($_FILES['attachment'])) {
+			$filename = date("YmdHis") . "." . pathinfo($_FILES['attachment']['name'])['extension'];
+			$filepath = PICS_FOLDER_PATH . $filename;
+			if (move_uploaded_file($_FILES['attachment']['tmp_name'], $filepath)) {
+				$picfrmt = "<br><a href='file.php?i=" . $filename . "'>Get attachment [" . $_FILES['attachment']['size'] . "B]</a>";
+			}
+		}
+		$msgfrmt = "<p><b>" . date("H:i m.d") . " by " . $username . ":</b><br>" . $msg . $picfrmt . "</p>" . "\n";
+		$f = fopen(PLAIN_PATH, 'a');
+		flock($f, LOCK_EX);
+		fwrite($f, $msgfrmt);
+		flock($f, LOCK_UN);
+		fclose($f);
+	}
+	header("Location:index.php");
+	exit();
+}

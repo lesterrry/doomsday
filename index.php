05f1de10099c3f915e85c5c02814b0e94d7b6f5f
Lester <me@aydar.media>
2022-03-19 22:36:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Goodbye</title>
@@ -15,7 +15,10 @@
 					background: #222;
 					font-family: Arial;
 				}
-			}
+				a {
+					color: #ff7f49;
+				}
+			}	
 		</style>
 	</head>
 	<body>
@@ -25,42 +28,38 @@
 				<?php
 					require __DIR__ . "/auth.php";
 					define("PLAIN_PATH", "PATH TO LOG FILE");
-					define("SHELL_COMMAND", "COMMAND TO EXECUTE");
 					$authstatus = "unauthorized";
 					$authok = false;
 					$key = "";
 					$username = "Unknown";
-					if (isset($_GET['key'])) {
-						$key = $_GET['key'];
-						$authstatus = "query key invalid";
+					if (isset($_POST['key'])) {
+						$key = $_POST['key'];
+						$authstatus = "POST key invalid";
+						header("Refresh:0");
 					}
 					else if (isset($_COOKIE['key'])) {
 						$key = $_COOKIE['key'];
 						$authstatus = "cookie key invalid";
 					}
+					else if (isset($_GET['key'])) {
+						$key = $_GET['key'];
+						$authstatus = "GET key invalid";
+					}
 					if (isset($key) && keyvalid($key)) {
 						$authok = true;
 						$username = getusername($key);
-						if ($authstatus == "query key invalid") {
-							$authstatus = "authorized via query key as " . $username;
+						if ($authstatus == "POST key invalid") {
+							$authstatus = "authorized via POST key as " . $username;
+							setcookie("key", $key);
+						}
+						else if ($authstatus == "GET key invalid") {
+							$authstatus = "authorized via GET key as " . $username;
 							setcookie("key", $key);
 						} else {
 							$authstatus = "authorized via cookie key as " . $username;
 						}
 					}
-					if (isset($_POST["message"])) {
-						$msg = $_POST["message"];
-						if(keyvalid($key)) {
-							$msgfrmt = "<p><b>" . date("m.d H:i") . " by " . $username . ":</b><br>" . $msg . "</p>\n";
-							$f = fopen(PLAIN_PATH, 'a');
-							flock($f, LOCK_EX);
-							fwrite($f, $msgfrmt);
-							flock($f, LOCK_UN);
-							fclose($f);
-							shell_exec(SHELL_COMMAND);
-						}
-						header("Refresh:0");
-					}
+					
 					echo("Last updated: " . date("H:i") . " • Auth status: " . $authstatus);
 				?>
 				<br>
@@ -71,21 +70,24 @@
 		<?php
 		if ($authok) {
 			echo("
-					<form method='post'>
+					<form action='message.php' method='post' enctype='multipart/form-data'>
+						<input type='hidden' name='key' value='" . $key . "' />
 						Message: <input type='text' name='message' maxlength=2000 size=100 placeholder='<2000 chars' required><br>
-						<input type='submit' value='Submit'>
+						<input type='hidden' name='MAX_FILE_SIZE' value='2097152' />
+						Attachment: <input type='file' name='attachment'><br>
+						<input type='submit' value='Send'>
 					</form>
-					<hr/>
+					<hr>
 				");
 			$content = file_get_contents(PLAIN_PATH);
 			echo($content);
 		} else {
 			echo("
-					<form method='get'> 
+					<form method='post'> 
 						Key: <input type='text' name='key' maxlength=100 size=100 required><br>
 						<input type='submit' value='Submit'>
 					</form>
-					<hr/>
+					<hr>
 				");
 		}
 		
